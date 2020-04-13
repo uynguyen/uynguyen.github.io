1815d95958c73768f853cf6cfe99f4f29793b991
Uy Nguyen <leesan.itus@gmail.com>
2020-04-13 21:49:59 +0700

    Done

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+git add .
+git commit -m "Done"
+git push -u origin master
+hexo generate
+cp -Rf ./public/* ../uynguyen.github.io
+cd ../uynguyen.github.io
+git add .
+git commit -m "deploy"
+git push
