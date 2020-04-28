c95d18d5c4ca1341f6ab58a1008a7452e43019a3
Uy Nguyen <leesan.itus@gmail.com>
2020-04-28 16:54:21 +0700

    Done

--- source/_posts/Building-your-personal-page-with-Hexo.md
+++ source/_posts/Building-your-personal-page-with-Hexo.md
@@ -2,6 +2,5 @@
 title: Building your personal page with Hexo
 date: 2020-04-27 20:56:51
 tags:
-thumbnail: /Post-Resources/Hexo/Cover.png
 ---
-<!-- ![]( "Hexo") -->
+![](/Post-Resources/Hexo/Cover.png "Hexo")
--- themes/icarus/layout/widget/profile.jsx
+++ themes/icarus/layout/widget/profile.jsx
@@ -34,9 +34,6 @@ class Profile extends Component {
                 <nav class="level">
                     <div class="level-item has-text-centered flex-shrink-1">
                         <div>
-                            <figure class="image is-128x128 mx-auto mb-2">
-                                <img class={avatarRounded ? 'is-rounded' : ''} src={avatar} alt={author} />
-                            </figure>
                             {author ? <p class="title is-size-4 is-block line-height-inherit">{author}</p> : null}
                             {authorTitle ? <p class="is-size-6 is-block">{authorTitle}</p> : null}
                             {location ? <p class="is-size-6 is-flex justify-content-center">
